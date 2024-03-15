0621ebab681a72aca1c28ff5a550c1ec337b0c4b
justin <justinmwathi32@gmail.com>
2024-03-15 19:26:51 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const names=["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names,event){
+    let newArray=[];
+    for(let i=0;i<names.length;i++){
+    newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }    
+    return newArray;
+    
+}
+writeCards(names,"surprise")
+
+
+function countDown(){
+    let countDown=0;
+    while(countDown<11){
+        console.log(countDown++)
+    }
+}
